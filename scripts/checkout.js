6281530a49421c47dce88215f0dfe31f54d81b2b
sonu gupta <sonugupta27032001@gmail.com>
2025-05-04 16:53:12 +0530

    save data and generate html for delivery options

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,8 +1,15 @@
 import {cart,removeFromCart} from '../data/cart.js';
 import { products } from '../data/products.js';
 import{formatCurrency}from './utils/money.js'
-
+import{hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js'
+import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js'
+import{deliveryOptions} from '../data/deliveryOption.js'
+//here the default export is happen wheenver we have to only export one core functionalliy then we use the default export 
+//while the upper one is name export form multiple export from file we use the name export
+//note all libraries has not esm version hence for some library we have to use script tag but dayjs has esm version so the upper one syntax we are able to use it
 let cartSummaryHTMl='';
+
+
 cart.forEach((cartItem)=>{
     const productId=cartItem.productId;
     let matchingProduct ;
@@ -12,13 +19,30 @@ cart.forEach((cartItem)=>{
 
         }
     })
+  const deliveryOptionId= cartItem.deliveryOptionId;
+  let deliveryOption ;
+  deliveryOptions.forEach((option)=>{
+    if(option.id===deliveryOptionId){
+      deliveryOption=option;
+    }
+
+  })
+  const today= dayjs();
+    const deliveryDate= today.add(
+      deliveryOption.deliveryDays,'days'
+
+
+    );
+ 
+    const dateString= deliveryDate.format('dddd, MMMM D');
+
   
 
     const html=
     `
     <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
-              Delivery date: Tuesday, June 21
+              Delivery date: ${dateString}
             </div>
 
             <div class="cart-item-details-grid">
@@ -29,7 +53,7 @@ cart.forEach((cartItem)=>{
                ${matchingProduct.name}
                 </div>
                 <div class="product-price">
-                 ${formatCurrency(matchingProduct.priceCents)}
+                 $${formatCurrency(matchingProduct.priceCents)}
                 </div>
                 <div class="product-quantity">
                   <span>
@@ -48,39 +72,9 @@ cart.forEach((cartItem)=>{
                 <div class="delivery-options-title">
                   Choose a delivery option:
                 </div>
-                <div class="delivery-option">
-                  <input type="radio" checked="" class="delivery-option-input" name="delivery-option-${matchingProduct.id}">
-                  <div>
-                    <div class="delivery-option-date">
-                      Tuesday, June 21
-                    </div>
-                    <div class="delivery-option-price">
-                      FREE Shipping
-                    </div>
-                  </div>
-                </div>
-                <div class="delivery-option">
-                  <input type="radio" class="delivery-option-input" name="delivery-option-${matchingProduct.id}">
-                  <div>
-                    <div class="delivery-option-date">
-                      Wednesday, June 15
-                    </div>
-                    <div class="delivery-option-price">
-                      $4.99 - Shipping
-                    </div>
-                  </div>
-                </div>
-                <div class="delivery-option">
-                  <input type="radio" class="delivery-option-input" name="delivery-option-${matchingProduct.id}">
-                  <div>
-                    <div class="delivery-option-date">
-                      Monday, June 13
-                    </div>
-                    <div class="delivery-option-price">
-                      $9.99 - Shipping
-                    </div>
-                  </div>
-                </div>
+                
+                
+                 ${deliveryOptionHTML(matchingProduct,cartItem)}
               </div>
             </div>
           </div>
@@ -100,3 +94,50 @@ document.querySelectorAll('.js-delete-link').forEach((link)=>{
 
     })
 })
+function deliveryOptionHTML(matchingProduct,cartItem){
+  let html=''
+  deliveryOptions.forEach((deliveryOption)=>{
+    const today= dayjs();
+    const deliveryDate= today.add(
+      deliveryOption.deliveryDays,'days'
+
+
+    );
+ 
+    const dateString= deliveryDate.format('dddd, MMMM D');
+
+    
+    const priceString = (deliveryOption.priceCents===0) ?'FREE':`${formatCurrency(deliveryOption.priceCents)} -`
+    const isChecked = deliveryOption.id===cartItem.deliveryOptionId;
+    html+=
+    `
+        <div class="delivery-option">
+                      <input type="radio" 
+                      ${isChecked?'checked':''}
+                      class="delivery-option-input" name="delivery-option-${matchingProduct.id}">
+                      <div>
+                        <div class="delivery-option-date">
+                      ${dateString}
+                        </div>
+                        <div class="delivery-option-price">
+                          ${priceString} Shipping
+                        </div>
+           </div>
+           </div>
+    `
+
+  })
+  return html;
+
+}
+
+//why we use external libaries
+// ->let us share code
+// ->save time
+// ->avoid duplicating work
+//esm version 
+// esm=ecmaScript Module (EcmaScript = javaScript)
+// it is another name of javascript which works with the javascript module
+
+
+
